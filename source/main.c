36aed297c22726deab2a3b13c027d6284a2f3616
aceinetx <mouselin82@gmail.com>
2025-06-20 16:48:56 +0000

    It's working!

--- source/main.c
+++ source/main.c
@@ -4,48 +4,61 @@
 #include <string.h>
 
 // Include the main libnx system header, for Switch development
+#include "../submodules/wpp-oss/onelibwpp.c"
 #include <switch.h>
 
 // Main program entrypoint
-int main(int argc, char* argv[])
-{
-    // This example uses a text console, as a simple way to output text to the screen.
-    // If you want to write a software-rendered graphics application,
-    //   take a look at the graphics/simplegfx example, which uses the libnx Framebuffer API instead.
-    // If on the other hand you want to write an OpenGL based application,
-    //   take a look at the graphics/opengl set of examples, which uses EGL instead.
-    consoleInit(NULL);
+int main(int argc, char *argv[]) {
+  // This example uses a text console, as a simple way to output text to the
+  // screen. If you want to write a software-rendered graphics application,
+  //   take a look at the graphics/simplegfx example, which uses the libnx
+  //   Framebuffer API instead.
+  // If on the other hand you want to write an OpenGL based application,
+  //   take a look at the graphics/opengl set of examples, which uses EGL
+  //   instead.
+  consoleInit(NULL);
 
-    // Configure our supported input layout: a single player with standard controller styles
-    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+  // Configure our supported input layout: a single player with standard
+  // controller styles
+  padConfigureInput(1, HidNpadStyleSet_NpadStandard);
 
-    // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
-    PadState pad;
-    padInitializeDefault(&pad);
+  // Initialize the default gamepad (which reads handheld mode inputs as well as
+  // the first connected controller)
+  PadState pad;
+  padInitializeDefault(&pad);
 
-    // Other initialization goes here. As a demonstration, we print hello world.
-    printf("Hello World!\n");
+  // Other initialization goes here. As a demonstration, we print hello world.
+  printf("Running sample code...\n");
+  wppLexer *lexer =
+      wpp_lexer_new("fn main; println \"Hello, from watermelon++!\"; nf;");
+  wppExec *exec = wpp_exec_new(lexer);
+  wpp_exec_run(exec);
+  if (*exec->error) {
+    printf("wpp: %s\n", exec->error);
+  }
 
-    // Main loop
-    while (appletMainLoop())
-    {
-        // Scan the gamepad. This should be done once for each frame
-        padUpdate(&pad);
+  wpp_lexer_free(lexer);
+  wpp_exec_free(exec);
 
-        // padGetButtonsDown returns the set of buttons that have been
-        // newly pressed in this frame compared to the previous one
-        u64 kDown = padGetButtonsDown(&pad);
+  // Main loop
+  while (appletMainLoop()) {
+    // Scan the gamepad. This should be done once for each frame
+    padUpdate(&pad);
 
-        if (kDown & HidNpadButton_Plus)
-            break; // break in order to return to hbmenu
+    // padGetButtonsDown returns the set of buttons that have been
+    // newly pressed in this frame compared to the previous one
+    u64 kDown = padGetButtonsDown(&pad);
 
-        // Your code goes here
+    if (kDown & HidNpadButton_Plus)
+      break; // break in order to return to hbmenu
 
-        // Update the console, sending a new frame to the display
-        consoleUpdate(NULL);
-    }
+    // Your code goes here
 
-    // Deinitialize and clean up resources used by the console (important!)
-    consoleExit(NULL);
-    return 0;
+    // Update the console, sending a new frame to the display
+    consoleUpdate(NULL);
+  }
+
+  // Deinitialize and clean up resources used by the console (important!)
+  consoleExit(NULL);
+  return 0;
 }
